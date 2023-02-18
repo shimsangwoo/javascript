c6e7b30856687eaa95013e34860b5ba01449b98e
sangwoo <sw_shim@hanmail.net>
2023-02-18 16:25:01 +0900

    asdf

--- tutorial/02.color/app.js
+++ tutorial/02.color/app.js
@@ -1,13 +1,13 @@
 // console.log('hello');
 const bg = ['orange', 'blue', 'red', 'black', 'yellow', 'green']
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, a, b, c, d, e, f]
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 'a', 'b', 'c', 'd', 'e', 'f']
 const hexBtn = document.getElementById('hexBtn')
-const bgBtn = document.getElementById('gbBtn')
+const bgBtn = document.getElementById('bgBtn')
 const color = document.querySelector('.color')
 
-console.log(bg)
-console.log(bgBtn)
-console.log(color)
+// console.log(bg)
+// console.log(bgBtn)
+// console.log(color)
 
 // console.log(Math.PI)
 
@@ -29,10 +29,10 @@ bgBtn.addEventListener('click', () => {
 hexBtn.addEventListener('click', () => {
   let hexColor = '#'
   for (let i = 0; i < 6; i++) {
-    hexcolor += hex[getHexNum()]
+    hexColor += hex[getHexNum()]
   }
 })
 
-document.body.style.backgroundColor = hexcolor
+document.body.style.backgroundColor = hexColor
 color.textContent = hexColor
 console.log(hexcolor)
