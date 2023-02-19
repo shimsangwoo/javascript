0229005ff5e43b761dc948d4f626e055d1225bf2
sangwoo <sw_shim@hanmail.net>
2023-02-19 09:48:59 +0900

    20230219 오전

--- tutorial/02.color/03 modal/app2.js
+++ tutorial/02.color/03 modal/app2.js
@@ -1,1 +1,11 @@
-console.log('asdfasdfasdf')
+const content = document.querySelector('.content')
+const modal = document.querySelector('.modal')
+const btns = document.querySelectorAll('.btn') //
+
+btns.forEach(() => {
+  /*function() 하고 같다.*/
+  btn.addEventListener('click', function () {
+    content.classList.toggle('active')
+    modal.classList.toggle('active')
+  })
+})
